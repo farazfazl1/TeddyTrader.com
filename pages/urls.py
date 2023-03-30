e9cd74d8b068fb3ced591bf06cf1392cf7d21696
farazfazl1 <farazfbusiness@gmail.com>
2023-03-30 15:03:46 -0700

    changes

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from pages import views
 
+app_name = "pages"
+
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('services/', views.services, name='services'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
+
 def home(req):
     return render(req, 'pages/home.html')
+
+
+def about(req):
+    return render(req, 'pages/about.html')
+
+
+def contact(req):
+    return render(req, 'pages/contact.html')
+
+
+def services(req):
+    return render(req, 'pages/services.html')
